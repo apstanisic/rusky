420f76ff04eb3a1b8dc12347d0556d8d0d7d14f1
Aleksandar Stanisic <aleksandar@tuta.io>
2023-03-25 23:18:35 +0100

    Hook works

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ use std::{
     fs::{self, create_dir_all, File, OpenOptions},
     io::{self, Write},
     os::unix::prelude::PermissionsExt,
-    path::{Path, PathBuf},
+    path::PathBuf,
     process::{self, Command, Output},
     vec,
 };
@@ -15,23 +15,12 @@ fn git(command: Vec<&str>) -> io::Result<Output> {
 }
 
 pub fn install(dest_dir: Option<String>) -> () {
-    let dir_str = match dest_dir {
-        Some(v) => v,
-        None => ".husky".to_string(),
-    };
-    let dir = PathBuf::from(dir_str.clone());
-    let absolute = if dir.is_absolute() {
-        dir
-    } else {
-        env::current_dir().unwrap().join(dir)
-    };
+    let husky_dir = get_absolute_path(
+        &dest_dir.unwrap_or(".husky".to_string()), //
+    );
+    let root_dir = husky_dir.clone().parent().unwrap().to_path_buf();
 
-    let root = absolute.clone();
-    let root = root.parent().unwrap().to_str().clone().unwrap();
-    // let root = absolute.clone().parent().unwrap().to_str().unwrap();
-
-    let env_husky = env::var("HUSKY").unwrap_or("".to_string());
-    if env_husky == "0" {
+    if env::var("HUSKY").unwrap_or("".to_string()) == "0" {
         println!("HUSKY env variable is set to 0, skipping install");
         return;
     }
@@ -47,33 +36,36 @@ pub fn install(dest_dir: Option<String>) -> () {
     //     absolute.par to_str().unwrap(),
     //     env::current_dir().unwrap().to_str().unwrap()
     // );
-    if !absolute.starts_with(env::current_dir().unwrap()) {
+    if !root_dir.starts_with(env::current_dir().unwrap()) {
         println!(".. not allowed (see {})", help_url);
         process::exit(1);
     }
 
-    let git_folder = format!("{}/.git", root);
-    if !Path::new(&git_folder).exists() {
+    let git_folder = root_dir.join(".git");
+    if !git_folder.exists() {
         println!(".git can't be found (see {})", help_url);
         process::exit(1);
     }
 
     let err_text = "Problem installing hooks.";
-    let husky_underscore = format!("{}/_", absolute.to_str().unwrap());
-    create_dir_all(husky_underscore).expect(err_text);
 
-    let git_ignore_path = format!("{}/_/.gitignore", absolute.to_str().unwrap());
-    println!("{}", &git_ignore_path);
+    let husky_underscore_folder = husky_dir.clone().join("_");
+    create_dir_all(husky_underscore_folder.clone()).expect(err_text);
+
+    let git_ignore_path = husky_underscore_folder.join(".gitignore");
     let mut git_ignore_file = File::create(&git_ignore_path).unwrap();
     git_ignore_file.write_all(b"*").expect(err_text);
 
-    let mut husky_sh_file =
-        File::create(format!("{}/_/husky.sh", absolute.to_str().unwrap())).expect(err_text);
+    let husky_sh_file_path = husky_underscore_folder.join("husky.sh");
+    let mut husky_sh_file = File::create(husky_sh_file_path).expect(err_text);
+
     husky_sh_file
         .write_all(shell_script::HUSKY_SHELL_SCRIPT.trim().as_bytes())
         .expect(err_text);
 
-    git(vec!["config", "core.hooksPath", &dir_str]).expect(err_text);
+    let binding = husky_dir.clone();
+    let git_check = binding.to_str().unwrap();
+    git(vec!["config", "core.hooksPath", &git_check]).expect(err_text);
 
     println!("Hooks installed successfully");
 }
@@ -82,35 +74,45 @@ pub fn uninstall() -> () {
     git(vec!["config", "--unset", "core.hooksPath"]).expect("Problem removing hooks");
 }
 
-pub fn set(file_path: String, cmd: String) {
+pub fn set(file: PathBuf, cmd: String) {
     let err_message = format!(
         "can't create hook, {} directory doesn't exist (try running husky install)",
-        &file_path
+        &file.to_str().unwrap()
     );
-    let file = std::path::Path::new(&file_path);
-    let dir = file.parent().expect(&err_message);
+    let dir = file.clone().parent().expect(&err_message).to_path_buf();
     if !dir.exists() {
         println!("{}", &err_message);
         process::exit(1);
     }
     let script_to_write = format!("{}\n{}", shell_script::USER_SCRIPT.trim(), &cmd);
 
-    File::open(file)
-        .unwrap()
+    println!("{}", file.to_str().unwrap());
+
+    File::create(file.clone())
+        .expect("Can't open file")
         .write_all(script_to_write.as_bytes())
-        .unwrap();
+        .expect("Can't write");
 
     fs::set_permissions(file, fs::Permissions::from_mode(0o0755)).expect(&err_message);
 }
 
 pub fn add(file_path: String, cmd: String) -> Result<(), Box<dyn std::error::Error>> {
-    if Path::new(&file_path).exists() {
-        let mut file = OpenOptions::new().append(true).open(&file_path)?;
-        file.write_all(format!("{}\n", cmd).as_bytes())?;
+    let path = get_absolute_path(&file_path);
+    if path.exists() {
+        let mut file = OpenOptions::new().append(true).open(path)?;
+        writeln!(file, "{}", cmd)?;
         println!("Updated {}", &file_path);
         return Ok(());
     } else {
-        set(file_path, cmd);
+        set(path, cmd);
         Ok(())
+    }
+}
+
+fn get_absolute_path(file_path: &str) -> PathBuf {
+    let dir = PathBuf::from(file_path);
+    match dir.is_absolute() {
+        true => dir,
+        false => env::current_dir().unwrap().join(dir),
     }
 }
